9b5887962d402e4c8b7460bd66bc95bee43c1163
wbdvlpmnt <jai.gokhale5219@gmail.com>
2024-01-06 13:46:34 -0600

    speed camera exerise

--- exercises/Exercises.cs
+++ exercises/Exercises.cs
@@ -61,4 +61,67 @@ public class Exercises()
 
     }
 
+    /*
+        Write a program and ask the user to enter the width and height of an image. Then tell if the image is landscape or portrait.
+    */
+
+    public static void Orientation()
+    {
+        Console.WriteLine("Enter Length: ");
+        var len = GetValue();
+
+        Console.WriteLine("Enter Width: ");
+        var wid = GetValue();
+
+        if (len > wid)
+        {
+            System.Console.WriteLine("Portrait Orientation");
+        }
+        else
+        {
+            System.Console.WriteLine("Landscape Orientation");
+        }
+
+    }
+
+    /*
+        Your job is to write a program for a speed camera. 
+        For simplicity, ignore the details such as camera, sensors, etc and focus purely on the logic.
+        Write a program that asks the user to enter the speed limit. Once set, the program asks for the speed of a car. 
+        If the user enters a value less than the speed limit, program should display Ok on the console. 
+        If the value is above the speed limit, the program should calculate the number of demerit points. 
+        For every 5km/hr above the speed limit, 1 demerit points should be incurred and displayed on the console. 
+        If the number of demerit points is above 12, the program should display License Suspended.
+    */
+
+    public static void SpeedCamera()
+    {
+        System.Console.WriteLine("Enter Speed Limit: ");
+        var speedLimit = GetValue();
+
+        System.Console.WriteLine("Enter Vehicle Speed: ");
+        var carSpeed = GetValue();
+
+        if (carSpeed < speedLimit)
+        {
+            System.Console.WriteLine("OK");
+            return;
+        }
+        else
+        {
+            var difference = carSpeed - speedLimit;
+            var demerritPoints = difference / 5;
+
+            if (demerritPoints > 12)
+            {
+                System.Console.WriteLine("Suspended");
+                return;
+            }
+
+            System.Console.WriteLine("Demerrit Points: {0}", demerritPoints);
+
+        }
+
+    }
+
 }
